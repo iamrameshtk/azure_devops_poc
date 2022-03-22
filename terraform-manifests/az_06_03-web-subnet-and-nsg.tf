939a35183e9d5e1ef49ab371140ae974993142a5
Ramesh Kasinathan <rameshkasinath@outlook.com>
2022-03-22 22:41:34 +0000

    Updated

--- terraform-manifests/az_06_03-web-subnet-and-nsg.tf
+++ terraform-manifests/az_06_03-web-subnet-and-nsg.tf
@@ -24,7 +24,6 @@ resource "azurerm_subnet_network_security_group_association" "web_subnet_nsg_ass
 ## Locals Block for Security Rules
 locals {
   web_inbound_ports_map = {
-    "100" : "80", # If the key starts with a number, you must use the colon syntax ":" instead of "="
     "110" : "443",
     "120" : "22"
   } 
